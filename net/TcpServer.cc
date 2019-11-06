531b31a3b11341f00f2d5dcfe2b29a023b9e2e3a
Pur <844840584@qq.com>
2019-11-06 11:29:02 +0800

    support win32

--- net/TcpServer.cc
+++ net/TcpServer.cc
@@ -1,10 +1,14 @@
 #include <iostream>
-#include <event.h>
 #include <event2/listener.h>
-#include <arpa/inet.h>
 #include "TcpServer.h"
 #include "TcpConnection.h"
-#include <iostream>
+#ifdef _WIN32
+#include <winsock.h>
+#endif
+#ifdef _UNIX
+#include <arpa/inet.h>
+#endif
+
 
 TcpServer::~TcpServer()
 {
@@ -12,37 +16,35 @@ TcpServer::~TcpServer()
 }
 
 void TcpServer::Start(short port) {
-	if (!m_eBase) {
-		m_eBase = event_base_new();
-	}
+    if (!m_eBase) {
+        m_eBase = event_base_new();
+    }
 
     sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_addr.s_addr = inet_addr("127.0.0.1");
     sin.sin_port = htons(port);
 
-	m_listener = evconnlistener_new_bind(
-		m_eBase,
-		TcpServer::accept_conn_cb,
-		nullptr,
-		LEV_OPT_LEAVE_SOCKETS_BLOCKING | LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE,
-		-1,
-		reinterpret_cast<sockaddr *>(&sin),
-		sizeof(sin)
-	);
+    m_listener = evconnlistener_new_bind(
+        m_eBase,
+        TcpServer::accept_conn_cb,
+        nullptr,
+        LEV_OPT_LEAVE_SOCKETS_BLOCKING | LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE,
+        -1,
+        reinterpret_cast<sockaddr *>(&sin),
+        sizeof(sin)
+    );
     auto methodName = event_base_get_method(m_eBase);
     std::cout << "libevent use: "<< methodName << std::endl;
-	event_base_dispatch(m_eBase);
+    event_base_dispatch(m_eBase);
 }
 
 void TcpServer::accept_conn_cb(evconnlistener *listener,evutil_socket_t fd,sockaddr *address, int socklen, void *arg)
 {
-    char addr[INET_ADDRSTRLEN];
-    auto *sin = reinterpret_cast<sockaddr_in *>(address);
-    inet_ntop(AF_INET, &sin->sin_addr, addr, INET_ADDRSTRLEN);
-    std::cout << "accept " << addr << std::endl;
-	TcpServer::getInstance()->OnNewConnection(fd);
+    auto *addr = reinterpret_cast<sockaddr_in *>(address);
+    std::cout << "accept addr:" << inet_ntoa(addr->sin_addr) << std::endl;
+    TcpServer::getInstance()->OnNewConnection(fd);
 }
 
 void TcpServer::OnNewConnection(evutil_socket_t fd)
